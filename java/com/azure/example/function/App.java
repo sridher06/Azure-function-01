03043e03f25dbb00a1ed019138552dafdced8760
sridher06 <sridhers@gmail.com>
2023-03-26 11:45:25 +0100

    initial code version

---
+++ java/com/azure/example/function/App.java
@@ -0,0 +1,13 @@
+package com.azure.example.function;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
